4fdec428dcd7f45daf1cba8a71739258e2acf4c6
AuthurightCode <74897259+authuright@users.noreply.github.com>
2024-01-21 15:03:35 +0000

    some change

---
+++ Python-Fundamental/OOP/the__init___method.py
@@ -0,0 +1,6 @@
+"""The __init__ method is a special method in python that it run automatically when we instance object"""
+
+""" self keyword : when we use it in class or function we can call any attribute or method anywhere and any time from class"""
+
+
+""" Sometime when we using __init__ in class it mean we use the overloading in oop concept"""
