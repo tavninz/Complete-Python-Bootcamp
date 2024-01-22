39289c4b37fe8b417fc5409fc606492dae8f3ed5
AuthurightCode <74897259+authuright@users.noreply.github.com>
2024-01-22 01:01:09 +0000

    error checking

---
+++ Python-Fundamental/8-Errors and Exceptions/Syntax-Error.py
@@ -0,0 +1,8 @@
+"""Syntax errors, also known as parsing errors, are perhaps the most common kind of complaint you get while you are still learning Python:"""
+
+
+
+while True:
+    print("Hello World)
+
+# SyntaxError: unterminated string literal (detected at line 6)
