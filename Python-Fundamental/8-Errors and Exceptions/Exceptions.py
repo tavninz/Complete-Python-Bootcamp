2a19a6a50c78bad0440fe53386e9fd0b9aaa4b6b
AuthurightCode <74897259+authuright@users.noreply.github.com>
2024-01-22 01:10:31 +0000

    update

---
+++ Python-Fundamental/8-Errors and Exceptions/Exceptions.py
@@ -0,0 +1,13 @@
+"""Even if a statement or expression is syntactically correct, it may cause an error when an attempt is made to execute it. Errors detected during execution are called exceptions and are not unconditionally fatal: you will soon learn how to handle them in Python programs. Most exceptions are not handled by programs, however, and result in error messages as shown here:"""
+
+
+# 10 * (1/0)
+
+# ZeroDivisionError: division by zero
+
+10 * (root/2)
+
+# NameError: name 'root' is not defined
+
+
+# Handle Error
