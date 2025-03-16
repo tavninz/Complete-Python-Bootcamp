515306340145ca1d10c27131d87c5655fd40eedd
tavninz <74897259+tavninz@users.noreply.github.com>
2025-03-16 13:39:50 +0700

    Section Variable

---
+++ Python-Fundamental/002_Lecture_Data_Types/Variable.py
@@ -0,0 +1,7 @@
+'''
+Variable in Python is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing. Every value in Python has a datatype. Different data types in Python are Numbers, List, Tuple, Strings, Dictionary, etc. Variables can be declared by any name or even alphabets like a, aa, abc, etc.
+'''
+
+name = "Jonh Doe"
+age = 25
+print(name,age)
